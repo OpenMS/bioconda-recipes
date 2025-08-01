9b27b2fa9a53b3d064e093df05528ef6dc249974
Avni2000 <avnibadiwale@gmail.com>
2025-08-01 04:48:02 -0500

    Fixed Qt6 issue on Fedora

--- recipes/openms-meta/build.sh
+++ recipes/openms-meta/build.sh
@@ -23,6 +23,7 @@ fi
 
 # not sure if needed. CMake should take care of that.
 LDFLAGS='-Wl,-rpath,${RPATH}'
+# Note: Cmake could not find Qt6Config.cmake on fedora without adding those prefix paths
 
 cmake .. \
   -DOPENMS_GIT_SHORT_REFSPEC="release/${PKG_VERSION}" \
@@ -31,7 +32,7 @@ cmake .. \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
   -DCMAKE_MACOSX_RPATH=ON \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_PREFIX_PATH=${PREFIX}:${CONDA_PREFIX}:"${CONDA_PREFIX}/lib/cmake/Qt6/" \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_RPATH=${RPATH} \
   -DCMAKE_INSTALL_NAME_DIR="@rpath" \
@@ -40,6 +41,7 @@ cmake .. \
   -DHAS_XSERVER=OFF \
   -DENABLE_TUTORIALS=OFF \
   -DWITH_GUI=OFF \
+  -DWITH_PARQUET=ON \
   -DBOOST_USE_STATIC=OFF \
   -DBoost_NO_BOOST_CMAKE=ON \
   -DBoost_ARCHITECTURE="-x64" \
